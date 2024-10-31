c5dce8a5451d5a8bab317dec6c2f86f3331f9fdc
minhha2k3 <hadep7a@gmail.com>
2024-10-31 15:48:33 +0700

    feat: add slide component

---
+++ script.js
@@ -0,0 +1,39 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Thay đổi 2000 thành thời gian bạn muốn giữ mỗi slide
+}
+
+function plusSlides(n) {
+  showSlidesWithIndex(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlidesWithIndex(slideIndex = n);
+}
+
+function showSlidesWithIndex(n) {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
